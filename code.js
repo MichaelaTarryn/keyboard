2e4d4ff80ac5dcf4554a0c212482525154c862ba
Michaela <Michaela-13>
2022-06-07 15:59:03 +0200

    first

---
+++ code.js
@@ -0,0 +1,11 @@
+let letter = "";
+
+function letterPop(letter){
+    document.getElementById('content').value += letter;
+
+}
+
+function clearSum(){
+ sum = "";
+   document.getElementById("content").value = sum;
+   }
